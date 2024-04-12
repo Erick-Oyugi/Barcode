e33b6d2e3d068d7bbc1d30cb0aa2a990c3827676
Erick Oyugi <erick.oyugi@CreditBank.co.ke>
2024-04-12 17:11:46 +0300

    Removed certs

--- server.js
+++ server.js
@@ -16,20 +16,24 @@ const path_to_cert = process.env.CERT_PATH
 
 
 function bootstrap(){
-   const environment = process.env.NODE_ENV;
-   var httpsOptions= {
-   key: fs.readFileSync(`${path_to_key}`),
-   cert: fs.readFileSync(`${path_to_cert}`),
-};
+//    const environment = process.env.NODE_ENV;
+//    var httpsOptions= {
+//    key: fs.readFileSync(`${path_to_key}`),
+//    cert: fs.readFileSync(`${path_to_cert}`),
+// };
 
 
 
-var server = https.createServer(httpsOptions, app).listen(port, ()=>{
-   console.log(`QRcode Backend running on  port ${port}`)
-})
-
+// var server = https.createServer(httpsOptions, app).listen(port, ()=>{
+//    console.log(`QRcode Backend running on  port ${port}`)
+// })
 
 // connectDB()
+
+app.listen(port, function(err){
+   if (err) console.log("Error in server setup")
+   console.log("Server listening on Port", port);
+})
 
 }
 
